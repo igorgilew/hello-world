b0f5682bb06659eb212a2d5f891a65d094626917
Alyona <Alyona-gil@mail.ru>
2021-05-19 23:43:39 +0500

    Создала ClassMult

---
+++ ConsoleApp1/ClassMult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class ClassMult
+    {
+        public int x { set; get; }
+        public int y { set; get; }
+        public int Mult()
+        {
+            return x * y;
+        }
+    }
+}
